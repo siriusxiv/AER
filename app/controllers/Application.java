b4c4898f2cc051190e0a4e69f6011d70b3efd942
royerjohan <johan.royer@eleves.ec-nantes.fr>
2014-03-21 14:21:05 +0100

    suite de la creation du squelette

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -20,8 +20,8 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
-
-import views.html.index;
+import play.data.*;
+import views.html.*;
 
 public class Application extends Controller {
 
@@ -32,66 +32,66 @@ public class Application extends Controller {
     /*Page de login ou d'inscription */
     
     public static Result identification() {
-    	return ok(identification.render());
+    	return ok( identification.render());
     }
     
     public static Result inscription() {
-    	return ok(inscription.render());
+    	return ok( inscription.render());
     }
     
     public static Result retrouverMotDePasse() {
-    	return ok(retrouverMotDePasse.render());
+    	return ok( retrouverMotDePasse.render());
     }
     
     /*Pages de l'utilisateur  */
     	
     public static Result menuUtilisateur() {
-    	return ok(menuUtilisateur.render());
+    	return ok( menuUtilisateur.render());
     }
     
     public static Result informationsPersonnelles() {
-    	return ok(informationsPersonnelles.render());
+    	return ok( informationsPersonnelles.render());
     }
     
     public static Result consulterDonnees() {
-    	return ok(consulterDonnees.render());
+    	return ok( consulterDonnees.render());
     }
     
     public static Result historique() {
-    	return ok(historique.render());
+    	return ok( historique.render());
     }
     
     
     /* Pages de l'expert  */
     
     public static Result menuExpert() {
-    	return ok(menuExpert.render());
+    	return ok( menuExpert.render());
     }
     
     public static Result temoignagesAValider() {
-    	return ok(temoignagesAValider.render());
+    	return ok( temoignagesAValider.render());
     }
     
     public static Result temoignagesValides() {
-    	return ok(temoignagesValides.render());
+    	return ok( temoignagesValides.render());
     }
     
     /*Pages de l'admin */
     
     public static Result menuAdmin() {
-    	return ok(menuAdmin.render());
+    	return ok( menuAdmin.render());
     }
     
-    public static Result assignerExpert() {
-    	return ok(AssignerExpert.render());
+    public static Result assignerExperts() {
+    	return ok( assignerExperts.render());
     }
     
     public static Result demandesInscription() {
-    	return ok(demandesInscription.render());
+    	return ok( demandesInscription.render());
     }
     
-    public static Result gererBasesDeDonneesInsectes() {
-    	return ok(gererBasesDeDonneesInsectes.render());
+    public static Result gererBaseDeDonneesInsectes() {
+    	return ok( gererBaseDeDonneesInsectes.render());
     }
     
     public static Result listeMembres() {
@@ -102,7 +102,7 @@ public class Application extends Controller {
     	return ok(listeTemoignages.render());
     }
     
-    /*Fiche de Témoignage */
+    /*Fiche de Temoignage */
     public static Result ficheDeTemoignage() {
     	return ok(ficheDeTemoignage.render());
     }
