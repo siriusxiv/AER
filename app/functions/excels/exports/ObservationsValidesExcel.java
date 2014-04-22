82f4877496a6c5d7318a88d57fd003e878029e82
royerjohan <johan.royer@eleves.ec-nantes.fr>
2014-04-22 14:21:16 +0200

    export excel marche

--- app/functions/excels/exports/ObservationsValidesExcel.java
+++ app/functions/excels/exports/ObservationsValidesExcel.java
@@ -8,8 +8,11 @@ import models.Espece;
 import models.Fiche;
 import models.FicheHasMembre;
 import models.Groupe;
+import models.InformationsComplementaires;
 import models.Observation;
 
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CreationHelper;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 
@@ -25,7 +28,7 @@ public class ObservationsValidesExcel extends Excel{
 		String titre= "Liste des Observations du groupe des "+groupe.groupe_nom;
 		Integer valide=Observation.VALIDEE;
 		List<Observation> observations= new ArrayList<Observation>();
-		if (membre_nom!=""){
+		if (membre_nom==""){
 			if (espece_id!=0){
 				Espece espece = Espece.find.byId(espece_id);
 		 observations= Observation.find.where()
@@ -82,6 +85,9 @@ public class ObservationsValidesExcel extends Excel{
 	rowtitre.createCell(10).setCellValue("Date de soumission");
 	rowtitre.createCell(11).setCellValue("Informations supplémentaires");
 	rowtitre.createCell(12).setCellValue("Date de validation");
+	CellStyle cellStyleDate = wb.createCellStyle();
+	CreationHelper creationHelper = wb.getCreationHelper();
+	cellStyleDate.setDataFormat(creationHelper.createDataFormat().getFormat("dd/mm/yyyy"));
 	int i = 2;
 	for(Observation observation : observations){
 		Row row = sheet.createRow(i);
@@ -99,20 +105,79 @@ public class ObservationsValidesExcel extends Excel{
 		row.createCell(11);
 		row.createCell(12);
 		sheet.getRow(i).getCell(0).setCellValue(observation.observation_id);
-		sheet.getRow(i).getCell(1).setCellValue("noms membres");
+		StringBuilder membres = new StringBuilder();
+		List<FicheHasMembre> mp = observation.observation_fiche.getFicheHasMembre();
+		for(int j = 0 ; j<mp.size()-1 ; j++){
+			membres.append(mp.get(j).membre);
+			membres.append(", ");
+		}
+		if(!mp.isEmpty())
+			membres.append(mp.get(mp.size()-1).membre);
+		else
+			membres.append("et al.");
+		sheet.getRow(i).getCell(1).setCellValue(membres.toString());
 		sheet.getRow(i).getCell(2).setCellValue(observation.observation_espece.espece_nom);
 		sheet.getRow(i).getCell(3).setCellValue(observation.observation_determinateur);
 		sheet.getRow(i).getCell(4).setCellValue(observation.observation_commentaires);
 		sheet.getRow(i).getCell(5).setCellValue(observation.observation_fiche.fiche_date.getTime());
+		sheet.getRow(i).getCell(5).setCellStyle(cellStyleDate);
 		sheet.getRow(i).getCell(6).setCellValue(observation.observation_fiche.fiche_lieudit);
-		sheet.getRow(i).getCell(7).setCellValue("commune");
+		if(observation.observation_fiche.fiche_commune!=null)
+		{
+		sheet.getRow(i).getCell(7).setCellValue(observation.observation_fiche.fiche_commune.ville_nom_aer);
+		}
 		sheet.getRow(i).getCell(8).setCellValue(observation.observation_fiche.fiche_utm.utm);
 		sheet.getRow(i).getCell(9).setCellValue(observation.observation_fiche.fiche_memo);
 		sheet.getRow(i).getCell(10).setCellValue(observation.observation_fiche.fiche_memo);
-		sheet.getRow(i).getCell(11).setCellValue(observation.observation_fiche.fiche_date_soumission.getTime());
-		sheet.getRow(i).getCell(12).setCellValue("infos supplémentaires");
+		sheet.getRow(i).getCell(11).setCellValue(observation.observation_fiche.fiche_date_soumission);
+		sheet.getRow(i).getCell(11).setCellStyle(cellStyleDate);
+		
+		StringBuilder infos = new StringBuilder();
+		List<InformationsComplementaires> ic= observation.getInfos();
+		for(InformationsComplementaires complements : ic){
+			if (complements.informations_complementaires_nombre_de_specimens!=null){
+				infos.append(complements.informations_complementaires_nombre_de_specimens);
+				infos.append(" ");
+			}else infos.append("? ");
+			if (complements.informations_complementaires_stade_sexe!=null)
+			{infos.append(complements.informations_complementaires_stade_sexe.stade_sexe_intitule);
+			infos.append(", ");
+			}
+		}
+		sheet.getRow(i).getCell(12).setCellValue(infos.toString());
+		/*
+		Integer nombre = complements.informations_complementaires_nombre_de_specimens;
+		if(nombre==null)
+			row.createCell(8).setCellValue("?");
+		else
+			row.createCell(8).setCellValue(nombre);
+		row.createCell(9).setCellValue(complements.informations_complementaires_stade_sexe.stade_sexe_intitule);
+		StringBuilder membres = new StringBuilder();
+		List<FicheHasMembre> fhms = fiche.getFicheHasMembre();
+		for(int j = 0 ; j<fhms.size()-1 ; j++){
+			membres.append(fhms.get(j).membre);
+			membres.append(", ");
+		}
+		if(!fhms.isEmpty())
+			membres.append(fhms.get(fhms.size()-1).membre);
+		else
+			membres.append("et al.");
+		row.createCell(10).setCellValue(membres.toString());*/
 		i++;
 	}
+
+	sheet.autoSizeColumn(1);
+	sheet.autoSizeColumn(2);
+	sheet.autoSizeColumn(3);
+	sheet.autoSizeColumn(4);
+	sheet.autoSizeColumn(5);
+	sheet.autoSizeColumn(6);
+	sheet.autoSizeColumn(7);
+	sheet.autoSizeColumn(8);
+	sheet.autoSizeColumn(9);
+	sheet.autoSizeColumn(10);
+	sheet.autoSizeColumn(11);
+	sheet.autoSizeColumn(12);
 	}
 
 }
